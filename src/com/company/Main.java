cb707c564734c7e11d4fa917ef8d853f55c6b27b
jb838 <jacob.blasek1220@gmail.com>
2018-11-05 11:20:07 -0500

    please help

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
-
+import java.util.*;
+import java.io.*;
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException{
+        Scanner sf = new Scanner(new File("Imports.txt"));
+
+        int maxIndx = -1;
+        String text[] = new String[1000];
+
+        while(sf.hasNext()) {
+
+            maxIndx++;
+            text[maxIndx] = sf.nextLine();
+
+        }
+
+        sf.close();
+
+        for (int j = 0; j <= maxIndx; j++){
+            System.out.println(text[j]);
+        }
     }
 }
---
+++ src/com/company/Student.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by jb838 on 11/5/18.
+ */
+public class Student {
+    private int abs;
+    private double grade;
+
+    public Student(int abs, double grade) {
+        int a = abs;
+        double g = grade;
+    }
+
+
+}
